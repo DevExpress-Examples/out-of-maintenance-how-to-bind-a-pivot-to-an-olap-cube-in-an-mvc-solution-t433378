1ddfd9a11545e842e80cac379920614ed40c586a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PivotGridPartial()
+        {
+            return PartialView("_PivotGridPartial");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to bind a pivot to an OLAP cube in an MVC solution
+
+
+<p>This example demonstrates how to bind an MVCxPivotGrid component to an OLAP source using the <a href="https://documentation.devexpress.com/#AspNet/DevExpressWebMvcPivotGridExtension_BindToOLAPtopic">PivotGridExtension.BindToOLAP</a> method. </p>
+
+<br/>
+
+
